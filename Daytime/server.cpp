529ba8b71fea0b9934d7cb72e19c81ca49844e99
karnitin2 <112693502+karnitin2@users.noreply.github.com>
2023-01-28 15:36:04 +0300

    Update server.cpp

--- Daytime/server.cpp
+++ Daytime/server.cpp
@@ -1,58 +1,62 @@
-#include <netinet/in.h>
 #include <iostream>
-#include <arpa/inet.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <cstring>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
 using namespace std;
-
-int main(int argc, char* argv[]) {
-struct sockaddr_in {
- short sin_family;
- unsigned short sin_port;
- struct in_addr sin_addr;
- char sin_zero[8];
-};
-struct in_addr {
- unsigned long s_addr;
-};
-
-int s = socket(AF_INET, SOCK_DGRAM, 0);
-if (s == -1) {
-    cout << "Socket error\n";
-}
-
-sockaddr_in * self_addr = new (sockaddr_in);
-self_addr->sin_family = AF_INET;
-self_addr->sin_port = htons(44214);
-self_addr->sin_addr.s_addr = inet_addr("127.0.0.1");
-
-int b = bind(s,(const sockaddr*) self_addr,sizeof(sockaddr_in));
-if(b == -1) {
-    cout << "Binding error\n";
-    return 1;
-}
-while(true) {
-    char connect[50];
-    time_t now = time(0);
-    tm *ltm = localtime(&now);
-    string day, monday, hour, minute, second;
-    string daytime;
-    ltm->tm_mday < 10 ? day = '0' + to_string(ltm->tm_mday) : day = to_string(ltm->tm_mday);
-    ltm->tm_mon < 10 ? monday = '0' + to_string(ltm->tm_mon + 1) : monday = to_string(ltm->tm_mon + 1);
-    ltm->tm_hour < 10 ? hour = '0' + to_string(ltm->tm_hour) : hour = to_string(ltm->tm_hour);
-    ltm->tm_min < 10 ? minute = '0' + to_string(ltm->tm_min) : minute = to_string(ltm->tm_min);
-    ltm->tm_sec < 10 ? second = '0' + to_string(ltm->tm_sec) : second = to_string(ltm->tm_sec);
-    daytime = day + '.' + monday + '.' + to_string(ltm->tm_year + 1900) + ' ' + hour + ':' + minute + ':' + second;
-    char daytime_msg[sizeof(daytime)];
-    strcpy(daytime_msg, daytime.c_str());
-    sockaddr_in * client_addr = new sockaddr_in;
-    socklen_t len = sizeof (sockaddr_in);
-    if(recvfrom(s, connect, sizeof(connect), 0, (sockaddr*)(client_addr), &len)) {
-        cout << "Successfull client connection!\n";
-        sendto(s, daytime_msg, sizeof(daytime_msg), 0, (sockaddr*)(client_addr), len);
-        cout << "Daytime was sent to client!\n";
+int main()
+{
+    int listening = socket(AF_INET, SOCK_STREAM, 0);
+    if (listening == -1) {
+        cerr << "Can't create a socket! Quitting" << endl;
+        return -1;
     }
-}
-close(s);
-return 0;
-}
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(33333);
+    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
+    bind(listening, (sockaddr*)&hint, sizeof(hint));
+    listen(listening, SOMAXCONN);
+    sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+    int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+    if (clientSocket != 4) {
+       cerr << "Can't connect! Quitting" << endl;
+       return -1;
+       close(clientSocket);
+    }
+    char host[NI_MAXHOST];
+    char service[NI_MAXSERV];
+    memset(host, 0, NI_MAXHOST);
+    memset(service, 0, NI_MAXSERV);
+    if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service,
+NI_MAXSERV, 0) == 0) {
+       cout << host << " connected on port " << service << endl;
+    } else {
+       inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+       cout << host << " connected on port " << ntohs(client.sin_port) << endl;
+    }
+    close(listening);
+    char buf[4096];
+    awhile (true) {
+       memset(buf, 0, 4096);
+       int bytesReceived = recv(clientSocket, buf, 4096, 0);
+       if (bytesReceived == -1) {
+          cerr << "Error in recv(). Quitting" << endl;
+          break;
+       }
+       if (bytesReceived == 0) {
+          cout << "Client disconnected " << endl;
+          break;
+       }
+       cout << string(buf, 0, bytesReceived) << endl;
+       send(clientSocket, buf, bytesReceived + 1, 0);
+       }
+    }  
+ close(clientSocket);
+ return 0;
+ }
+
