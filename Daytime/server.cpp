d521e839a4eddc9f5bd3a2160419c38a9d743141
karnitin2 <112693502+karnitin2@users.noreply.github.com>
2023-01-28 15:44:45 +0300

    Update server.cpp

--- Daytime/server.cpp
+++ Daytime/server.cpp
@@ -1,62 +1,43 @@
 #include <iostream>
+#include <stdlib.h>
 #include <sys/types.h>
-#include <unistd.h>
 #include <sys/socket.h>
-#include <netdb.h>
-#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <stdio.h>
+#include <signal.h>
 #include <string.h>
-#include <string>
+#include <unistd.h>
 using namespace std;
 int main()
 {
-    int listening = socket(AF_INET, SOCK_STREAM, 0);
-    if (listening == -1) {
-        cerr << "Can't create a socket! Quitting" << endl;
-        return -1;
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd == -1)
+    {
+       perror("Socket Creation Has Failed: ");
+       exit(-1);
     }
-    sockaddr_in hint;
-    hint.sin_family = AF_INET;
-    hint.sin_port = htons(33333);
-    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
-    bind(listening, (sockaddr*)&hint, sizeof(hint));
-    listen(listening, SOMAXCONN);
-    sockaddr_in client;
-    socklen_t clientSize = sizeof(client);
-    int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
-    if (clientSocket != 4) {
-       cerr << "Can't connect! Quitting" << endl;
-       return -1;
-       close(clientSocket);
+    struct sockaddr_in addr;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(32214);
+    if (bind(fd, (const struct sockaddr *) &addr, sizeof(addr)) == -1)
+    {
+       perror("Listening Failed on the Socket: ");
+       exit(-1);
     }
-    char host[NI_MAXHOST];
-    char service[NI_MAXSERV];
-    memset(host, 0, NI_MAXHOST);
-    memset(service, 0, NI_MAXSERV);
-    if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service,
-NI_MAXSERV, 0) == 0) {
-       cout << host << " connected on port " << service << endl;
-    } else {
-       inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-       cout << host << " connected on port " << ntohs(client.sin_port) << endl;
+    struct sockaddr_in cliaddr;
+    socklen_t cliaddr_len = sizeof(cliaddr);
+    char buffer[100];
+    cout << "Server:\n";
+    while(1) {
+       string dt = "daytime";
+       char dtBuff[sizeof(dt)];
+       strcpy(dtBuff, dt.c_str());
+       recvfrom(fd, buffer, 100,0,(struct sockaddr *) &cliaddr, &cliaddr_len);
+       cout << "Client: " << buffer << endl;
+       sendto(fd, dtBuff, sizeof(dtBuff),0,(const struct sockaddr *) &cliaddr,
+cliaddr_len);
     }
-    close(listening);
-    char buf[4096];
-    awhile (true) {
-       memset(buf, 0, 4096);
-       int bytesReceived = recv(clientSocket, buf, 4096, 0);
-       if (bytesReceived == -1) {
-          cerr << "Error in recv(). Quitting" << endl;
-          break;
-       }
-       if (bytesReceived == 0) {
-          cout << "Client disconnected " << endl;
-          break;
-       }
-       cout << string(buf, 0, bytesReceived) << endl;
-       send(clientSocket, buf, bytesReceived + 1, 0);
-       }
-    }  
- close(clientSocket);
- return 0;
- }
-
+    return 0;
+}
